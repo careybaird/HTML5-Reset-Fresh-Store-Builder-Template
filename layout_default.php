5a3c168d70a1552a9a7664a5a53f4b658e5de8b2
Austin Gulati <contact@austingulati.com>
2011-07-27 11:54:11 -0400

    cleaned up layout

--- layout_default.php
+++ layout_default.php
@@ -1,31 +1,29 @@
 <!DOCTYPE HTML>
 <html>
+
 	<head>
 		<title><?= $page_title ?></title>
-		
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<meta name="keywords" content="<?= $page_keywords ?>" />
 		<meta name="description" content="<?= $page_description ?>" />
-		
 		<?php
 			if ($metanoindex)
 				echo '<meta name="robots" content="noindex,follow" />';
 			else
 				echo '<meta name="robots" content="index,follow" />';
 		?>
-
 		<link rel="canonical" href="<?= $canonicalurl ?>" />
-		
 		<script type="text/javascript">
 			if (top.location != self.location) top.location = self.location;
 			window.siteurl = '<?= URL_SITE ?>';
 		</script>
-
 		<link rel="stylesheet" type="text/css" href="<?= URL_SITE ?>templates/<?= TEMPLATE_FOLDER ?>/styles/style.css">	
-		
-		<?php require('elements/analytics.php'); ?>
-		<?php require('elements/rss.php'); ?>
+		<?php
+			require('elements/analytics.php');
+			require('elements/rss.php');
+		?>
 	</head>
+	
 	<body<?= $body_onload ?>>
 		<?php
 			if (ADMIN_LOGGED_IN && file_exists(PATH_TEMPLATES.TEMPLATE_FOLDER.'/elements/admin_stripe_'.$adminstripename.'.php'))
@@ -40,11 +38,15 @@
 			
 			<header>
 				<div id="header-left">
-					<?php require('elements/logo.php') ?>
+					<?php
+						require('elements/logo.php');
+					?>
 				</div>
 				
 				<div id="header-right">
-					<?php require('elements/cart.php') ?>
+					<?php
+						require('elements/cart.php');
+					?>
 				</div>
 				
 				<div class="clearall"></div>
@@ -52,39 +54,41 @@
 			
 			<div id="main">
 				<div id="content-right">
-					<?php require('elements/breadcrumb.php') ?>
-					<?php require('elements/messages.php') ?>
-					<?= $pagecontents ?>
 					<?php
+						require('elements/breadcrumb.php');
+						require('elements/messages.php');
+						echo $pagecontents;
 						if (ADMIN_LOGGED_IN && $editinplacename != '')
-						{
 							require('elements/'.$editinplacename.'_editinplacejs.php');
-						}
 					?>
 				</div>
+				
 				<div id="menu-left">
-					<?php require('elements/searchbox.php') ?>
-					<?php require('elements/categoriesbox.php') ?>
-					<?php require('elements/brandsbox.php') ?>
-					<?php require('elements/filtersbox.php') ?>
 					<?php
+						require('elements/searchbox.php');
+						require('elements/categoriesbox.php');
+						require('elements/brandsbox.php');
+						require('elements/filtersbox.php');
 						foreach ($sideboxes as $sidebox)
-						{
 							require('elements/sidebox_'.$sidebox['type'].'.php');
-						}
-					?>	
-					<?php require('elements/amazonlogo.php') ?>
+						require('elements/amazonlogo.php');
+					?>
+				</div>
+					
 				<div class="clearall"></div>		
 			</div>
 
-			<div id="footer">
-				<?php require('elements/securepayments.php') ?>
-				<?php require('elements/footerlinks.php') ?>
-			</div>
+			<footer>
+				<?php
+					require('elements/securepayments.php');
+					require('elements/footerlinks.php');
+				?>
+			</footer>
 			
 		</div>
 		
 	<script type="text/javascript" src="<?= URL_SITE ?>templates/<?= TEMPLATE_FOLDER ?>/scripts/main.js"></script>
 	<?php if (isset($includescripts) && is_array($includescripts)) foreach ($includescripts as $script) echo "<script type=\"text/javascript\" src=\"{$script}\"></script>"; ?>	
 	</body>
+	
 </html>
