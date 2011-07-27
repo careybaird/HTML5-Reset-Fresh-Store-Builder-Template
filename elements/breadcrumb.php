e1299aad347de734e8d27cf184f783e3bf9890b8
Austin Gulati <contact@austingulati.com>
2011-07-27 14:26:34 -0400

    compressed CSS

--- elements/breadcrumb.php
+++ elements/breadcrumb.php
@@ -1,5 +1,6 @@
 <div id="breadcrumb">
 <?php
+	// How you want to handle more than one breadcrumb is up to you, but I think the default setting of just displaying them all is a little bit weird
 	foreach($breadcrumb as $crumb)
 	{
 		?>
@@ -7,10 +8,13 @@
 				<?php
 				$i = 0;
 				foreach($crumb as $item)
+				{
 					if ($item['url'])
-						echo "<a href=\"{$item['url']}\" class=\"breadcrumb-item breadcrumb-level-<?=$i?>\">{$item['name']}</a> >";
+						echo "<a href=\"{$item['url']}\" class=\"breadcrumb-item breadcrumb-level-{$i}\">{$item['name']}</a> >";
 					else
 						echo "<span class=\"breadcrumb-item breadcrumb-level-{$i}\">{$item['name']}</a>";
+					$i++;
+				}
 				?>
 			</div>
 		<?php
