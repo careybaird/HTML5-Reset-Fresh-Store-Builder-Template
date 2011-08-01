e5dddebdb605d7699ef6896105cacc8973a1a668
Austin Gulati <contact@austingulati.com>
2011-08-01 09:30:13 -0400

    added application stylesheet

--- layout_default.php
+++ layout_default.php
@@ -17,7 +17,8 @@
 			if (top.location != self.location) top.location = self.location;
 			window.siteurl = '<?= URL_SITE ?>';
 		</script>
-		<link rel="stylesheet" type="text/css" href="<?= URL_SITE ?>templates/<?= TEMPLATE_FOLDER ?>/styles/style.css">	
+		<link rel="stylesheet" type="text/css" href="<?= URL_SITE ?>templates/<?= TEMPLATE_FOLDER ?>/styles/style.css">
+		<link rel="stylesheet" type="text/css" href="<?= URL_SITE ?>templates/<?= TEMPLATE_FOLDER ?>/styles/application.css">	
 		<?php
 			require('elements/analytics.php');
 			require('elements/rss.php');
