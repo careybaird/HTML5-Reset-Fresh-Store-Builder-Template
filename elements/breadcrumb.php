84313faddf2c91524c13e86731fd02840412a5e2
Austin Gulati <contact@austingulati.com>
2011-08-08 12:01:39 -0400

    fixed validation issue

--- elements/breadcrumb.php
+++ elements/breadcrumb.php
@@ -1,6 +1,10 @@
 <div id="breadcrumb">
 <?php
-	// How you want to handle more than one breadcrumb is up to you, but I think the default setting of just displaying them all is a little bit weird
+	/*
+	* 	How you want to handle more than one breadcrumb is up to you, but I
+	* 	think the default setting of just displaying them all is a little bit weird
+	*	  since I would think that there should be only one line breadcrumbs
+	*/
 	foreach($breadcrumb as $crumb)
 	{
 		?>
@@ -12,7 +16,7 @@
 					if ($item['url'])
 						echo "<a href=\"{$item['url']}\" class=\"breadcrumb-item breadcrumb-level-{$i}\">{$item['name']}</a> > ";
 					else
-						echo "<span class=\"breadcrumb-item breadcrumb-level-{$i}\">{$item['name']}</a>";
+						echo "<span class=\"breadcrumb-item breadcrumb-level-{$i}\">{$item['name']}</span>";
 					$i++;
 				}
 				?>
