a5a252e7f4b1b21a82cd1a459d3bc4a88b7d50b3
Austin Gulati <contact@austingulati.com>
2011-08-10 11:31:57 -0400

    more basic styles

--- elements/cart.php
+++ elements/cart.php
@@ -1,11 +1,14 @@
-<a href="<?= URL_SITE ?><?= $pages['basket']['pagename'] ?>/" rel="nofollow">View Cart</a>
-<p><?= ($amazon->cart ? ($amazon->cart['totalquantity'] == 1 ? '1 item' : $amazon->cart['totalquantity'].' items') : '0 items') ?> - <?= formatProductPrice($amazon->cart['subtotal']) ?></p>
-<?php
-	if ($amazon->cart && $amazon->cart['totalquantity'] > 0 && $showcheckoutbutton)
-	{
-		?>
-				<a href="<?= URL_SITE ?><?= $pages['checkout']['pagename'] ?>/" rel="nofollow">
-					Checkout
-				</a>
-		<?php
-	}
+<div class="cart">
+	<a href="<?= URL_SITE ?><?= $pages['basket']['pagename'] ?>/" rel="nofollow">View Cart</a>
+	<p><?= ($amazon->cart ? ($amazon->cart['totalquantity'] == 1 ? '1 item' : $amazon->cart['totalquantity'].' items') : '0 items') ?> - <?= formatProductPrice($amazon->cart['subtotal']) ?></p>
+	<?php
+		if ($amazon->cart && $amazon->cart['totalquantity'] > 0 && $showcheckoutbutton)
+		{
+			?>
+					<a href="<?= URL_SITE ?><?= $pages['checkout']['pagename'] ?>/" rel="nofollow">
+						Checkout
+					</a>
+			<?php
+		}
+	?>
+</div>
