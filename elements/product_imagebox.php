1387d48174a23b81442111f83c47e815bb10f528
Austin Gulati <contact@austingulati.com>
2011-08-10 11:09:33 -0400

    fixing validation errors

--- elements/product_imagebox.php
+++ elements/product_imagebox.php
@@ -1,20 +1,20 @@
 <div id="product-image-box">
-	<a href="<?= URL_SITE ?>img/<?= $product['pagename'] ?>_<?= $mainimage['id'] ?>_500.jpg" rel="lightbox[product]" title="<?= $product['title'] ?>">
-		<img class="product-photo" src="<?= URL_SITE ?>img/<?= $product['pagename'] ?>_<?= $mainimage['id'] ?>_250.jpg" alt="<?= $product['title'] ?>" />
+	<a href="<?= URL_SITE ?>img/<?= $product['pagename'] ?>_<?= $mainimage['id'] ?>_500.jpg" title="<?= $product['title'] ?>">
+		<img class="product-image" src="<?= URL_SITE ?>img/<?= $product['pagename'] ?>_<?= $mainimage['id'] ?>_250.jpg" alt="<?= $product['title'] ?>" />
 	</a>
 
 	<?php
 		if (sizeof($images) > 0) 
 		{
 			echo '<span class="image-enlarge">Click images to enlarge</span>';
-				foreach ($images as $image)
-				{
-					?>
-						<a href="<?= URL_SITE ?>img/<?= $product['pagename'] ?>_<?= $image['id'] ?>_500.jpg" rel="lightbox[product]" title="<?= $product['title'] ?>">
-							<img src="<?= URL_SITE ?>img/<?= $product['pagename'] ?>_<?= $image['id'] ?>_50.jpg" class="image-thumbnail" />
-						</a>				
-					<?php
-				}
+			foreach ($images as $image)
+			{
+				?>
+					<a href="<?= URL_SITE ?>img/<?= $product['pagename'] ?>_<?= $image['id'] ?>_500.jpg" title="<?= $product['title'] ?>">
+						<img src="<?= URL_SITE ?>img/<?= $product['pagename'] ?>_<?= $image['id'] ?>_50.jpg" class="image-thumbnail" />
+					</a>				
+				<?php
+			}
 		}
 	?>
 	<br class="clearall" />
--- product.php
+++ product.php
@@ -7,7 +7,7 @@
 		require_once('elements/product_pricebox.php');
 		require_once('elements/product_imagebox.php');
 		require_once('elements/product_descriptionbox.php');
-		require_once('elements/product_azdescriptionbox.php');
+		//require_once('elements/product_azdescriptionbox.php');
 		require_once('elements/product_reviewbox.php');
 		require_once('elements/product_attributesbox.php');
 		require_once('elements/product_secondarydescriptionbox.php');
