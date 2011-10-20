d7cf0d7cf80eb93b0ebac96abf95486269886c82
Austin Gulati <contact@austingulati.com>
2011-10-20 19:41:24 -0400

    jQuery'fy these too

--- elements/adminstripe/adminstripe_contact.php
+++ elements/adminstripe/adminstripe_contact.php
@@ -2,7 +2,7 @@
 	<?php loadTemplateElement('adminstripe', 'left'); ?>
 	
 	<div id="admin-stripe-right">
-		<a href="#" id="stripe_togglefields" onclick="Effect.BlindDown('page_editinplacebox'); return false;">
+		<a href="#" id="stripe_togglefields" onclick="$('#page_editinplacebox').show('fast','swing'); return false;">
 			<img src="<?= URL_SITE ?>templates/<?= TEMPLATE_FOLDER ?>/icons/admin_quickeditshow.png" alt="<?=gTT('ADMINSTRIPE_QUICK_EDIT')?>" title="<?=gTT('ADMINSTRIPE_QUICK_EDIT')?>" />
 	</div>
 	
