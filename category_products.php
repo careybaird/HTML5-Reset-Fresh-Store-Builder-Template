ed9205f627196f94d05a03989802f1f717d54617
Austin Gulati <contact@austingulati.com>
2011-10-24 16:44:23 -0400

    This didn't work

--- category_products.php
+++ category_products.php
@@ -4,7 +4,7 @@
 ?>
 
 <h2 id="category-title" class="category-title"><?= $thiscategory['title'] ?></h2>
-<div id="category-maintext" class="category-description"><?= $thiscategory['description'] ?></div>
+<div id="category-description" class="category-description"><?= $thiscategory['description'] ?></div>
 <div class="clearall"></div>
 
 <?php
--- elements/category/category_editinplacejs.php
+++ elements/category/category_editinplacejs.php
@@ -8,7 +8,7 @@
 		save_url = '/ajax/category_savefield.php?id=<?= $thiscategory['id'] ?>';
 	
 		$('#category-title').editInPlace({url: save_url, default_text: '(<?=gTT('EDITINPLACE_ADD_TEXT')?>)'});
-		$('#category-maintext').editInPlace({url: save_url, field_type: 'textarea', default_text: '(<?=gTT('EDITINPLACE_ADD_DESCRIPTION')?>)'});
+		$('#category-description').editInPlace({url: save_url, field_type: 'textarea', default_text: '(<?=gTT('EDITINPLACE_ADD_DESCRIPTION')?>)'});
 		$('#category-descriptionbottom').editInPlace({url: save_url, field_type: 'textarea', default_text: '(<?=gTT('EDITINPLACE_ADD_DESCRIPTION')?>)'});
 		$('#category-linktitle').editInPlace({url: save_url, default_text: '(<?=gTT('EDITINPLACE_ADD_TEXT')?>)'});
 		$('#category-metadescription').editInPlace({url: save_url, field_type: 'textarea', default_text: '(<?=gTT('EDITINPLACE_ADD_METADESCRIPTION')?>)'});
