67ed8113d0ac4bb6607bae9d14357dadcfa919bd
Austin Gulati <contact@austingulati.com>
2011-11-14 20:52:14 -0500

    We need these files too, other changes as well

---
+++ elements/404/404_editinplacejs.php
@@ -0,0 +1,17 @@
+<?php
+	// This is temporarily, it will be moved to the controller as soon as edits start on the core files
+	global $page;
+?>
+<script type="text/javascript">
+$(document).ready(function()
+{
+	save_url = '/ajax/page_savefield.php?id=<?=$page['id']?>';
+	
+	$('#page-title').editInPlace({url: save_url, default_text: '(<?=gTT('EDITINPLACE_ADD_TEXT')?>)'});
+	$('#page-linktitle').editInPlace({url: save_url, default_text: '(<?=gTT('EDITINPLACE_ADD_TEXT')?>)'});
+	$('#page-maintext').editInPlace({url: save_url, form_type: 'textarea', default_text: '<em><?=gTT('EDITINPLACE_ADD_DESCRIPTION')?></em>'});
+	$('#page-metadescription').editInPlace({url: save_url, form_type: 'textarea', default_text: '<em><?=gTT('EDITINPLACE_ADD_METADESCRIPTION')?></em>'});
+	$('#page-metakeywords').editInPlace({url: save_url, default_text: '(<?=gTT('EDITINPLACE_ADD_TEXT')?>)'});
+	$('#page-metatitle').editInPlace({url: save_url, default_text: '(<?=gTT('EDITINPLACE_ADD_TEXT')?>)'});
+});
+</script>
---
+++ elements/404/404_editinplaceoptions.php
@@ -0,0 +1,21 @@
+<?php
+	// This is temporarily, it will be moved to the controller as soon as edits start on the core files
+	global $page;
+?>
+<div id="page_editinplacebox" class="editinplace-box" style="display:none;">
+	<div class="editinplace-close">
+		<a href="#" onclick="$('#page_editinplacebox').hide('slow','swing'); return false;">
+			<img src="<?= URL_SITE ?>templates/<?= TEMPLATE_FOLDER ?>/icons/admin_quickedithide.png" />
+		</a>
+	</div>
+
+	<p><span class="editinplace-label"><?=gTT('EDITINPLACE_PAGETITLE')?></span> <span class="editinplace-value" id="page-title"><?= $page['title'] ?></span></p>
+	<p><span class="editinplace-label"><?=gTT('EDITINPLACE_LINKTITLE')?></span> <span class="editinplace-value" id="page-linktitle"><?= $page['linktitle'] ?></span></p>
+	<p><span class="editinplace-label"><?=gTT('EDITINPLACE_MAINTEXT')?></span> <span class="editinplace-value" id="page-maintext"><?= htmlentities($page['maintext']) ?></span></p>
+	<p><span class="editinplace-label"><?=gTT('EDITINPLACE_METADESCRIPTION')?></span> <span class="editinplace-value" id="page-metadescription"><?= $page['metadescription'] ?></span></p>
+	<p><span class="editinplace-label"><?=gTT('EDITINPLACE_METAKEYWORDS')?></span> <span class="editinplace-value" id="page-metakeywords"><?= $page['metakeywords'] ?></span></p>
+	<p><span class="editinplace-label"><?=gTT('EDITINPLACE_METATITLE')?></span> <span class="editinplace-value" id="page-metatitle"><?= $page['metatitle'] ?></span></p>
+	<p><span class="editinplace-label"><?=gTT('EDITINPLACE_PAGENAME')?></span> <span class="editinplace-value" id="page-pagename"><?= $page['pagename'] ?></span></p>
+	
+	<br class="clearall" />
+</div>
