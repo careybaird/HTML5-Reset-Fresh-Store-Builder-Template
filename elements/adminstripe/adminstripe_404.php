8d04dd779593b0eb8474fa309abf9d57c638946a
Austin Gulati <contact@austingulati.com>
2011-11-14 20:34:11 -0500

    We need this too

---
+++ elements/adminstripe/adminstripe_404.php
@@ -0,0 +1,21 @@
+<?php
+	// This is temporarily, it will be moved to the controller as soon as edits start on the core files
+	global $page;
+?>
+<div id="admin-stripe">
+	<?php loadTemplateElement('adminstripe', 'left'); ?>
+	
+	<div id="admin-stripe-right">
+		<a href="#" id="stripe_togglefields" onclick="$('#page_editinplacebox').show('slow','swing'); return false;">
+			<?=getTemplateImage('ADMINSTRIPE_QUICK_EDIT','','',gTT('ADMINSTRIPE_QUICK_EDIT'))?>
+		</a>
+		<a href="<?= URL_SITE ?>freshadmin/page/edit/<?= $page['id'] ?>/">
+			<?=getTemplateImage('ADMINSTRIPE_EDIT','','',gTT('ADMINSTRIPE_EDIT'))?>
+		</a>
+		<a href="<?= URL_SITE ?>freshadmin/page/list/">
+			<?=getTemplateImage('ADMINSTRIPE_LIST_PAGES','','',gTT('ADMINSTRIPE_LIST_PAGES'))?>
+		</a>
+	</div>
+	
+	<br class="clearall" />
+</div>
